915ddf6beb50ab785b6bec5eddc0fffb00687f00
Miguel Ectil <87404714+Miguel-ectil@users.noreply.github.com>
2025-08-07 10:56:12 -0300

    Update exercicio_01.py
    [UPD] Corrigindo o código do exercício

--- exercicio_01.py
+++ exercicio_01.py
@@ -4,22 +4,21 @@ app = Flask(__name__)
 
 @app.route('/amostra', methods=['POST'])
 def amostra():
-  dados = request.get_json()
-  idade = dados.get("idade", 0)
+    dados = request.get_json()
+    idade = dados.get("idade", 0)
 
-  if idade > 5 and idade < 7:
-    categoria = "Infantil"
-#   elif idade > 8 < 10:
-#     categoria = "Iniciando"
-#   elif idade > 11 < 13:
-#     categoria = "Juvenil"
-#   elif idade > 14 < 17:
-#     categoria = "Júnior"
-  else:  
-    categoria = "Maior de idade"
-    print(categoria)
+    if 5 < idade < 7:
+        categoria = "Infantil"
+    elif 8 < idade < 10:
+        categoria = "Iniciando"
+    elif 11 < idade < 13:
+        categoria = "Juvenil"
+    elif 14 < idade < 17:
+        categoria = "Júnior"
+    else:
+        categoria = "Maior de idade"
 
-    return jsonify("Resultado", categoria)
+    return jsonify({"resultado": categoria})
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port='8000', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
